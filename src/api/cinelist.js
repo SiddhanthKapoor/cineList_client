16c8d8edfc09cf18ae473078182924d664edb334
siddhanthkapoor <kapoorsiddhanth15@gmail.com>
2025-10-24 22:57:45 +0530

    Changed Important

--- src/api/cinelist.js
+++ src/api/cinelist.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/movies';
+const API_URL = process.env.REACT_APP_API_URL + '/api/movies';
 
 const cinelist = axios.create({
   baseURL: API_URL,
